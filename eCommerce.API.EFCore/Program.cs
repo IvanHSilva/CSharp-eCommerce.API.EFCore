913f035ae5d2ec761ea3deef2285e10bd44e3e2e
IvanHSilva <ivanhenriquessilva@gmail.com>
2022-11-29 19:39:55 -0300

    UserModel

--- eCommerce.API.EFCore/Program.cs
+++ eCommerce.API.EFCore/Program.cs
@@ -1,14 +1,16 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+#region ConfigureService()
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+#endregion
 
 var app = builder.Build();
 
+#region Configure()
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment()) {
     app.UseSwagger();
@@ -22,3 +24,4 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+#endregion
---
+++ eCommerce.Models/User.cs
@@ -0,0 +1,29 @@
+﻿namespace eCommerce.Models {
+    public class User {
+        // Attributes
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? EMail { get; set; }
+        public string? Gender { get; set; }
+        public string? RG { get; set; }
+        public string? CPF { get; set; }
+        public string? Filiation { get; set; }
+        public string? Situation { get; set; }
+        public DateTime RegDate { get; set; }
+
+        // Constructors
+        public User() { }
+
+        public User(int id, string name, string? eMail, string? gender, string? rG, string? cPF, string? filiation, string? situation, DateTime regDate) {
+            Id = id;
+            Name = name;
+            EMail = eMail;
+            Gender = gender;
+            RG = rG;
+            CPF = cPF;
+            Filiation = filiation;
+            Situation = situation;
+            RegDate = regDate;
+        }
+    }
+}
