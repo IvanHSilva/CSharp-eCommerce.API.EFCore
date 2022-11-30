bf6f4c2664689f2f3ab674c791e9c523e21f90e5
IvanHSilva <ivanhenriquessilva@gmail.com>
2022-11-30 18:43:15 -0300

    UserRepository

---
+++ eCommerce.API.EFCore/Repositories/UserRepository.cs
@@ -0,0 +1,30 @@
+﻿using eCommerce.Models;
+
+namespace eCommerce.API.EFCore.Repositories {
+    public class UserRepository : IUserRepository {
+
+        // Fake user list
+        public static List<User> _db = new List<User>();
+
+        public List<User> GetUsers() {
+            return _db;
+        }
+
+        public User GetUser(int id) {
+            return _db.Find(u => u.Id == id)!;
+        }
+
+        public void InsertUser(User user) {
+            _db.Add(user);
+        }
+
+        public void UdateUser(User user) {
+            _db.Remove(GetUser(user.Id));
+            _db.Add(user);
+        }
+
+        public void DeleteUser(int id) {
+            _db.Remove(GetUser(id));
+        }
+    }
+}
