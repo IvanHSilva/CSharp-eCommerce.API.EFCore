98ba07c1e9a81977fb3edb5a134a8e9e705c6538
IvanHSilva <ivanhenriquessilva@gmail.com>
2023-01-05 14:46:01 -0300

    eCommerceOffice DBContext

--- eCommerce.Office/eCommerceOfficeContext.cs
+++ eCommerce.Office/eCommerceOfficeContext.cs
@@ -1,10 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using eCommerce.Office.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace eCommerce.Office {
-    internal class eCommerceOfficeContext {
+    public class eCommerceOfficeContext : DbContext {
+
+        public DbSet<Employee>? Employees { get; set; }
+        public DbSet<Sector>? Sectors { get; set; }
+        public DbSet<Vehicle>? Vehicles { get; set; }
+        public DbSet<Team>? Teams { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+            optionsBuilder.UseSqlServer("Data Source=SERVIDOR\\SQLSERVER;Initial Catalog=eCommerceOffice;Integrated Security=True;");
+        }
     }
 }
